88dd6e0f245145e2555cbddbc099f7b304efd74b
Katie <37264185+actuallyykatie@users.noreply.github.com>
2018-11-23 16:18:32 +0300

    db update

--- db.py
+++ db.py
@@ -32,15 +32,29 @@ _users = {
 
 }
 
-
 _music = {
     'song1': {
         'author': 'Monetochka',
         'title': 'Нет монет',
         'album cover': 'link_will_be_here'
+    },
+    'song2': {
+        'author': 'onuka',
+        'title': 'vidlik',
+        'album cover': 'link_will_be_here'
+    },
+    'song3': {
+        'author': 'son lux',
+        'title': 'lost it to trying ',
+        'album cover': 'link_will_be_here'
+    },
+    'song4': {
+        'author': 'onuka',
+        'title': 'vsesvit',
+        'album cover': 'link_will_be_here'
     }
 
- }
+}
 
 
 def get_user(username):
@@ -56,6 +70,7 @@ for login, user_data in _users.items():
     _new_element.update(user_data)  # add data from the 2nd dct to this one
     _user_list.append(_new_element)
 
+
 def get_users_by_name(q):
     results = []
     for user in _user_list:
@@ -64,6 +79,7 @@ def get_users_by_name(q):
     return results
 
 
+# music
 _music_list = []
 for uid, song_data in _music.items():
     _new_element = {'uid': uid}
@@ -71,9 +87,9 @@ for uid, song_data in _music.items():
     _music_list.append(_new_element)
 
 
-def get_song(q):
-    results = []
+def get_song_by_author(q):
+    results_songs = []
     for song in _music_list:
         if q.lower() in song['author'].lower():
-            results.append(song)
-    return results
+            results_songs.append(song)
+    return results_songs
